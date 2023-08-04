8f43d962fa59a5455ba912c4fb9d34be9afa269f
AslaN <73108781+Aslan-Taleb@users.noreply.github.com>
2023-08-04 13:53:29 +0100

    Update You_either_know_XOR_you_don't.py

--- Introduction To Cryptohack/You_either_know_XOR_you_don't.py
+++ Introduction To Cryptohack/You_either_know_XOR_you_don't.py
@@ -1,13 +1,6 @@
-encrypted_hex = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
-encrypted = bytes.fromhex(encrypted_hex)
-
-def is_crypto(text):
-        search_text = "crypto"
-        search_bytes = search_text.encode('utf-8')
-        if search_bytes in text:
-             return True
-        else:
-             False
-all_possible_keys = [key for key in range(256)]
-for i in all_possible_keys:
-      
+from pwn import xor
+flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
+# it says 'myXORke+y...'
+xor(flag, 'crypto{'.encode())
+# trying until it's work
+print(f"The Flag : {xor(flag, 'myXORkey'.encode())}") 
