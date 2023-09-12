51309991b59a21eeafd48e89c409f501c5c87627
Aslan Taleb <aslantalebselim@gmail.com>
2023-09-12 20:56:35 +0200

    add

--- S1.Java JEE/Test/src/main/java/com/example/dossier/leTest.java
+++ S1.Java JEE/Test/src/main/java/com/example/dossier/leTest.java
@@ -2,25 +2,32 @@ package com.example.dossier;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(value = "/hello-servlet")
+@WebServlet()
 public class leTest extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hi ! \nThis is my first servlet!\n";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        /*
+        version des tipeu
         response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        */
+        this.getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+
     }
 
     public void destroy() {
